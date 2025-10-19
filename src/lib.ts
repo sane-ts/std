6cfa2df14fdc98238de87480eb4b871f46746e5c
Valerii Petryniak <valerii15298@gmail.com>
2025-10-20 00:23:56 +0300

    feat: export enum module from lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,7 @@
 export * from "#fetch.ts";
 export * from "#json.ts";
 export * from "#parse-csv.ts";
+export * from "#enum.ts";
 export type * from "#types.ts";
 
 export function raise(err: Error): never {
